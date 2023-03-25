b366251ee5ee8d653f61f09124d9a7c985d498cd
ErikKoop <erikkoop@web.de>
2023-03-25 13:17:13 +0100

    25.03.2023

---
+++ BewerbungsAufgabe.php
@@ -0,0 +1,36 @@
+
+
+<?php 
+
+  include('./template/head.php');      // Headbereich
+  include('./template/input.php');     // Eingabe
+
+  if (isset($_POST["inputZahl"])) {    // Prüft, ob die Eingabezahl schon vorhanden ist!
+  function berechnungRoemisch(){      // Berechnung der Aufgabe
+  
+  $eingabeZahl = $_POST['inputZahl']; // Eingabezahl als Veriable 
+  $roemisch = ["M","CM",  "D",  "CD",   "C","XC", "L", "VX", "X", "IX","V","IV","I"];      // Array für die Buchstaben
+  $wert = ["1000","900", "500", "400", "100","90", "50","40", "10","9", "5","4", "1"];   // Array für die Werte der Buchstaben
+  $ergebnis [] = "";                  // Array für das Ergnis
+  echo $eingabeZahl;
+  echo " ist in römischen Zahlen: ";
+  if($eingabeZahl>3000 || $eingabeZahl < 1){
+    echo "Bitte wählen Sie erneut eine Zahl zwischen 1 und 3000";
+  }  
+  else{
+  for ($i = 0; $eingabeZahl > 0; $i++){   // For-Schleife läuft solange bis $i kleiner als die Eingabezahl ist
+  if ($eingabeZahl >= $wert[$i]){         // If-Abfrage Berechnung startet, falls die Eingabezahl größer/gleich als die erste Zahl vom Array $Wert ist. Falls sie kleiner ist, startet die Vorschleife einfach die zweite Runde und vergleicht es mit der zweiten Zahl vom Array $Wert ... etc.
+  $eingabeZahl -= $wert[$i];              // Wert wird von der Eingabe substrahiert
+  $ergebnis [$i] = $roemisch[$i];         // Array Ergebnis wird mit den Werten gefüllt
+  echo $ergebnis[$i];                     // Gibt den Wert aus 
+  $i -= 1;                                // Setzt $i zurück, um zu überprüfen, ob der immer noch größer ist als der selbe Werte. z.B. 200 ist >= als 100 und nach 200 - 100 trifft es danach nochmal zu.
+  }
+  }
+}
+ return $ergebnis;  // Liegt außerhalb der Schleife und gibt somit das Endergebnis aus der Funktion zurück, damit wir außerhalb der Funktion auf diesen Wert zurückgreifen können!
+}
+$ergebnis = berechnungRoemisch();   // Wir greifen auf den Wert aus der Funktion zu und betätigen sie hiermit auch.
+var_dump ($ergebnis);             // Habe ich ausgegraut, da ich Echo in der Forschleife verwende.
+}
+
+?>
---
+++ template/input.php
@@ -0,0 +1,15 @@
+
+
+<body class="d-block text-center justify-content-center">
+<div class="container-xxl text-center d-flex justify-content-center" >
+
+<form action='BewerbungsAufgabe.php' method='post'>
+  <div class="mb-1">
+    <label for="exampleInputEmail1" class="form-label"><h1>Römisch-Rechner</h1></label>
+    <input type="number" class="form-control" name="inputZahl">
+    <div id="eingabefeld" class="form-text">Geben Sie bitte eine Zahl zwischen 0 und 3000 ein</div>
+  </div>
+  <button type="submit" class="btn btn-primary">In römische Zahlen umwandeln</button>
+</form>
+</div>
+</body>
