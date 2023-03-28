0858f3bdac6dd1b21f1517b25bb6c6b845cbc864
ErikKoop <erikkoop@web.de>
2023-03-28 08:40:49 +0200

    Aufgabe fertig

--- BewerbungsAufgabe.php
+++ BewerbungsAufgabe.php
@@ -1,18 +1,18 @@
 <?php 
 
-  include('./template/head.php');      // Headbereich
-  include('./template/input.php');     // Eingabe
+  include('./template/head.php');         // Headbereich
+  include('./template/input.php');        // Eingabe
 
-  if (isset($_POST["inputZahl"])) {    // Prüft, ob die Eingabezahl schon vorhanden ist!
-  function berechnungRoemisch(){      // Funktion der Berechnung der Aufgabe
+  if (isset($_POST["inputZahl"])) {       // Prüft, ob die Eingabezahl schon vorhanden ist!
+  function berechnungRoemisch(){          // Funktion der Berechnung der Aufgabe
   
-  $eingabeZahl = $_POST['inputZahl']; // Eingabezahl als Variable 
-  $roemisch = ["M","CM",  "D",  "CD",   "C","XC", "L", "VX", "X", "IX","V","IV","I"];      // Array für die Buchstaben
-  $wert = ["1000","900", "500", "400", "100","90", "50","40", "10","9", "5","4", "1"];   // Array für die Werte der Buchstaben
-  $ergebnis [] = "";                  // Array für das Ergebnis
+  $eingabeZahl = $_POST['inputZahl'];     // Eingabezahl als Variable 
+  $roemisch = ["M","CM",  "D",  "CD",   "C","XC", "L", "VX", "X", "IX","V","IV","I"];        // Array für die Buchstaben
+  $wert = ["1000","900", "500", "400", "100","90", "50","40", "10","9", "5","4", "1"];       // Array für die Werte der Buchstaben
+  $ergebnis [] = "";                      // Array für das Ergebnis
  
-  if($eingabeZahl > 3000 || $eingabeZahl < 1){  // Begrenzung der Zahleingabe
-    echo "Bitte wählen Sie erneut eine Zahl zwischen 1 und 3000"; //Ausgabe
+  if($eingabeZahl > 3000 || $eingabeZahl < 1){                                               // Begrenzung der Zahleingabe
+    echo "Bitte wählen Sie erneut eine Zahl zwischen 1 und 3000";                            //Ausgabe
   }  
   else{                                   // Hier läuft die Begrechnung los, falls die Begrenzung nicht zutrifft.
     echo $eingabeZahl;                    //Ausgabe
@@ -26,10 +26,10 @@
   }
   }
 }
- return $ergebnis;  // Liegt außerhalb der Schleife und gibt somit das Endergebnis aus der Funktion zurück, damit wir außerhalb der Funktion auf diesen Wert zurückgreifen können!
+ return $ergebnis;                        // Liegt außerhalb der Schleife und gibt somit das Endergebnis aus der Funktion zurück, damit wir außerhalb der Funktion auf diesen Wert zurückgreifen können!
 }
-$ergebnis = berechnungRoemisch();   // Wir greifen auf den Wert aus der Funktion zu und betätigen sie hiermit auch.
-//var_dump ($ergebnis);             // Habe ich ausgegraut, da ich Echo in der Forschleife verwende.
+$ergebnis = berechnungRoemisch();         // Wir greifen auf den Wert aus der Funktion zu und betätigen sie hiermit auch.
+//var_dump ($ergebnis);                   // Habe ich ausgegraut, da ich Echo in der Forschleife verwende.
 }
 
 ?>
